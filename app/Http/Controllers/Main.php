fff73e101f6bef3ddf9495f3c979fa93338d2593
wznuhidayat <nextwznu@gmail.com>
2022-03-04 02:25:45 +0700

    connect with api on standings

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -6,16 +6,78 @@ use Illuminate\Http\Request;
 
 class Main extends Controller
 {
+    public $temp_paginate;
     public function index()
     {
         return view('home_page');
     }
     public function Standings()
     {
-        return view('standings');
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://api-football-beta.p.rapidapi.com/standings?season=2021&league=39",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => [
+                "x-rapidapi-host: api-football-beta.p.rapidapi.com",
+                "x-rapidapi-key: 0b0b1d9f51mshb9c676c3765bc8cp1082f9jsn62369aac2aed"
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            // echo $response;
+            $result = json_decode($response, true);
+            $data['name_league'] = $result['response'][0]['league']['name'];
+            $data['season'] = $result['response'][0]['league']['season'];
+            $data['standings'] = $result['response'][0]['league']['standings'][0];
+            return view('standings', $data);
+        }
     }
     public function Matches()
     {
+        // $curl = curl_init();
+
+        // curl_setopt_array($curl, [
+        //     CURLOPT_URL => "https://api-football-beta.p.rapidapi.com/fixtures?to=2022-09-24&timezone=Asia%2FJakarta&season=2021&league=39&from=" . date('Y-m-d') . "",
+        //     CURLOPT_RETURNTRANSFER => true,
+        //     CURLOPT_FOLLOWLOCATION => true,
+        //     CURLOPT_ENCODING => "",
+        //     CURLOPT_MAXREDIRS => 10,
+        //     CURLOPT_TIMEOUT => 30,
+        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        //     CURLOPT_CUSTOMREQUEST => "GET",
+        //     CURLOPT_HTTPHEADER => [
+        //         "x-rapidapi-host: api-football-beta.p.rapidapi.com",
+        //         "x-rapidapi-key: 0b0b1d9f51mshb9c676c3765bc8cp1082f9jsn62369aac2aed"
+        //     ],
+        // ]);
+
+        // $response = curl_exec($curl);
+        // $err = curl_error($curl);
+
+        // curl_close($curl);
+
+        // // if ($err) {
+        // //     echo "cURL Error #:" . $err;
+        // // } else {
+        // //     echo $response;
+        // // }
+        // $result = json_decode($response,true);
+
+        // $this->temp_paginate = $result['response'][0]['league']['standings'][0];
         return view('matches');
     }
     public function Clubs()
--- resources/views/standings.blade.php
+++ resources/views/standings.blade.php
@@ -1,47 +1,60 @@
 @extends('main_template')
 @section('container')
-<section class="mt-32 mb-32">
-  <div class="container mx-auto px-6">
+    <section class="mt-32 mb-32">
+        <div class="container mx-auto px-6">
 
-    {{-- <div class="rounded-lg bg-slate-100 p-5"> --}}
-      <h1 class="text-5xl  mb-5 font-bold">Table</h1>
-      <table class="table-auto w-full ">
-        <thead class="animate-fade-in-down">
-          <tr class="top-[-400px] transition-all ease-in duration-500 rounded-lg">
-            <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">No</th>
-            <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">Logo</th>
-            <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">Name</th>
-            <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">MP</th>
-            <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">W</th>
-            <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">D</th>
-            <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">L</th>
-            <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">GF</th>
-            <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">GA</th>
-            <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">GD</th>
-            <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">Pts</th>
-            <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">Last</th>
-          </tr>
-        </thead>
-        <tbody class="animate-fade-in-up">
-          <?php for ($i=0; $i < 20; $i++) { ?>
-          <tr class="bg-gray-200">
-            <td class="px-3 py-3 text-center text-sm"><?= $i+1; ?></td>
-            <td class="px-3 py-3 text-center text-sm">x</td>
-            <td class="px-3 py-3 text-center text-sm">Manchester City</td>
-            <td class="px-3 py-3 text-center text-sm">21</td>
-            <td class="px-3 py-3 text-center text-sm">12</td>
-            <td class="px-3 py-3 text-center text-sm">42</td>
-            <td class="px-3 py-3 text-center text-sm">1</td>
-            <td class="px-3 py-3 text-center text-sm">33</td>
-            <td class="px-3 py-3 text-center text-sm">4</td>
-            <td class="px-3 py-3 text-center text-sm">31</td>
-            <td class="px-3 py-3 text-center text-sm">23</td>
-            <td class="px-3 py-3 text-center text-sm">--------</td>
-          </tr>
-          <?php } ?>
-        </tbody>
-      </table>
-    {{-- </div> --}}
-  </div>
-</section>
+            {{-- <div class="rounded-lg bg-slate-100 p-5"> --}}
+            <h1 class="text-5xl  mb-5 font-bold">Table</h1>
+            <table class="table-auto w-full ">
+                <thead class="animate-fade-in-down">
+                    <tr class="top-[-400px] transition-all ease-in duration-500 rounded-lg">
+                        <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">No</th>
+                        <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">Logo</th>
+                        <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">Name</th>
+                        <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">MP</th>
+                        <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">W</th>
+                        <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">D</th>
+                        <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">L</th>
+                        <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">GF</th>
+                        <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">GA</th>
+                        <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">GD</th>
+                        <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">Pts</th>
+                        <th class="px-3 py-3 bg-gray-800 text-white font-bold text-md ">Last</th>
+                    </tr>
+                </thead>
+                <tbody class="animate-fade-in-up">
+                    <?php foreach ($standings as $standing) : ?>
+                    <tr>
+                        <th>{{ $standing['rank'] }}</th>
+                        <td class="px-3 py-3 text-center text-sm"><img src="{{ $standing['team']['logo'] }}" alt=""
+                                class="w-8 h-w-8 object-cover"></td>
+                        <td class="px-3 py-3 text-center text-sm">{{ $standing['team']['name'] }}</td>
+                        <td class="px-3 py-3 text-center text-sm">{{ $standing['all']['played'] }}</td>
+                        <td class="px-3 py-3 text-center text-sm">{{ $standing['all']['win'] }}</td>
+                        <td class="px-3 py-3 text-center text-sm">{{ $standing['all']['draw'] }}</td>
+                        <td class="px-3 py-3 text-center text-sm">{{ $standing['all']['lose'] }}</td>
+                        <td class="px-3 py-3 text-center text-sm">{{ $standing['all']['goals']['for'] }}</td>
+                        <td class="px-3 py-3 text-center text-sm">{{ $standing['all']['goals']['against'] }}</td>
+                        <td class="px-3 py-3 text-center text-sm">
+                            {{ $st = $standing['all']['goals']['for'] - $standing['all']['goals']['against'] }}</td>
+                        <td class="px-3 py-3 text-center text-sm">{{ $standing['points'] }}</td>
+                        <?php $splts = str_split($standing['form']); ?>
+                        <td class="px-3 py-3 text-center text-sm">
+                            <?php foreach ($splts as $splt) : ?>
+                            <?php if ($splt == 'W') { ?>
+                            <ion-icon name="checkmark-circle-outline" class="text-green-600" size="small"></ion-icon>
+                            <?php } elseif ($splt == 'D') { ?>
+                            <ion-icon name="remove-circle-outline" class="text-yellow-600" size="small"></ion-icon>
+                            <?php } else { ?>
+                            <ion-icon name="close-circle-outline" class="text-red-600" size="small"></ion-icon>
+                            <?php } ?>
+                            <?php endforeach ?>
+                        </td>
+                    </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+            {{-- </div> --}}
+        </div>
+    </section>
 @endsection
