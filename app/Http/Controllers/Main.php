3ef5fd19228207c9c64dffb3520125c2cc3fb40b
wznuhidayat <nextwznu@gmail.com>
2022-03-10 02:29:16 +0700

    connect club menu with API

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -77,19 +77,15 @@ class Main extends Controller
         //     $this->temp_paginate = $result['response'];
 
         // }
-       
+
         return view('matches');
     }
     public function Clubs()
     {
-        return view('clubs');
-    }
-    public function Matchespage($page = 0,$limit=5)
-    {
         $curl = curl_init();
 
         curl_setopt_array($curl, [
-            CURLOPT_URL => "https://api-football-beta.p.rapidapi.com/fixtures?to=2022-09-24&timezone=Asia%2FJakarta&season=2021&league=39&from=" . date('Y-m-d') . "",
+            CURLOPT_URL => "https://api-football-beta.p.rapidapi.com/teams?league=39&season=2021",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_ENCODING => "",
@@ -111,7 +107,53 @@ class Main extends Controller
         if ($err) {
             echo "cURL Error #:" . $err;
         } else {
-            $result = json_decode($response,true);
+            $result = json_decode($response, true);
+            $club = $result['response'];
+            $count = count($club);
+            for ($i = 0; $i < $count; $i++) {
+                for ($j = $i + 1; $j < $count; $j++) {
+                    if ($club[$i]['team']['name'] > $club[$j]['team']['name']) {
+                        $temp = $club[$i];
+                        $club[$i] = $club[$j];
+                        $club[$j] = $temp;
+                    }
+                }
+            }
+            $data['total'] = $count;
+            $data['data'] = $club;
+            return view('clubs',$data);
+        }
+        
+        
+    }
+    public function Matchespage($page = 0, $limit = 5)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://api-football-beta.p.rapidapi.com/fixtures?to=2022-09-24&timezone=Asia%2FJakarta&season=2021&league=39&status=NS&from=" . date('Y-m-d') . "",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => [
+                "x-rapidapi-host: api-football-beta.p.rapidapi.com",
+                "x-rapidapi-key: 0b0b1d9f51mshb9c676c3765bc8cp1082f9jsn62369aac2aed"
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $result = json_decode($response, true);
             $fixture = $result['response'];
             $count = count($fixture);
             for ($i = 0; $i < $count; $i++) {
@@ -123,14 +165,13 @@ class Main extends Controller
                     }
                 }
             }
-            $startIndex = ($page - 1)*$limit;
+            $startIndex = ($page - 1) * $limit;
             $endIndex = $page * $limit;
 
             $data['total'] = $count;
-            $data['data'] = array_slice($fixture,$startIndex,$endIndex);
-            
+            $data['data'] = array_slice($fixture, $startIndex, $endIndex);
+
             return $data;
         }
-       
     }
 }
--- resources/views/clubs.blade.php
+++ resources/views/clubs.blade.php
@@ -9,19 +9,22 @@
                 <h2 class="text-slate-100 text-lg ">29 Des 2021</h2>
             </div> --}}
             <hr>
-            <div class="flex flex-wrap justify-between mt-3">
-                <?php for ($i=0; $i < 20; $i++) {  ?>
-                <div class="w-full md:w-72 h-72 bg-slate-100  py-8 flex flex-col justify-between rounded-lg shadow-md animate-fade-in-up mb-5">
-                    <img src="https://upload.wikimedia.org/wikipedia/id/0/0c/Liverpool_FC.svg" alt=""
-                        class="inline object-cover w-20 h-20 mx-auto mb-5 border-2 border-gray-800 rounded-full">
-                    <a href="" class="text-center"> Liverpool</a>
-                    <a href="" class="text-gray-600 text-center">Anfiled Stadium</a>
-                    <a href="" class="text-gray-600 text-center mt-5">
-                        <ion-icon name="arrow-forward-circle-outline" size="large"></ion-icon>
-                    </a>
-
+            <div class="bg-gray-800 p-3 mb-3 animate-fade-in-down rounded-lg flex justify-between">
+                <h2 class="text-slate-100 text-lg text-right">Season 2021/2022</h2>
                 </div>
-                <?php } ?>
+            <div class="flex flex-wrap justify-between mt-3">
+                <?php foreach ($data as $club) : ?>
+                    <div class="w-full md:w-72 h-72 bg-slate-100  py-8 px-8 flex flex-col justify-between rounded-lg shadow-md animate-fade-in-up mb-5">
+                        <img src="{{ $club['team']['logo'] }}" alt=""
+                            class="inline object-cover w-20 h-20 mx-auto mb-5 border-2 border-gray-800 rounded-full">
+                        <a href="" class="text-center"> {{ $club['team']['name'] }}</a>
+                        <a href="" class="text-gray-600 text-center">{{ $club['venue']['name'] }}</a>
+                        <a href="" class="text-gray-600 text-center mt-5 hover:text-black">
+                            <ion-icon name="arrow-forward-circle-outline" size="large"></ion-icon>
+                        </a>
+
+                    </div>
+                <?php endforeach ?>
             </div>
             {{-- </div> --}}
         </div>
